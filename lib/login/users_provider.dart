2248a4ad609acb401dc38c37a65afc419b808d76
grm.vera <grm.vera@yavirac.edu.ec>
2024-12-11 22:31:55 +0000

    bug: correcion de bug en el inicio de sesion

--- lib/login/users_provider.dart
+++ lib/login/users_provider.dart
@@ -4,10 +4,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class UsuarioProvider with ChangeNotifier {
   Map<String, dynamic>? _userData;
+  bool _isLoading = false; // Indica si los datos se están cargando
 
   Map<String, dynamic>? get userData => _userData;
+  bool get isLoading => _isLoading; // Getter para el estado de carga
 
   Future<void> fetchUserData() async {
+    _isLoading = true; // Cambia el estado a cargando
+    notifyListeners();
+
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       final snapshot = await FirebaseFirestore.instance
@@ -16,8 +21,10 @@ class UsuarioProvider with ChangeNotifier {
           .get();
       if (snapshot.exists) {
         _userData = snapshot.data();
-        notifyListeners();
       }
     }
+
+    _isLoading = false; // Finaliza el estado de carga
+    notifyListeners();
   }
 }
