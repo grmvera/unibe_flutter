705ae79bc255a485e3a8aaff0a83941ea176100a
grmvera <grm.vera@yavirac.edu.ec>
2025-01-14 15:44:25 -0500

    boton acad

--- lib/Widget/custom_drawer.dart
+++ lib/Widget/custom_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:unibe_app_control/login/login_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CustomDrawer extends StatelessWidget {
   final Map<String, dynamic> userData;
@@ -27,12 +28,17 @@ class CustomDrawer extends StatelessWidget {
               backgroundColor: Colors.grey[300],
               child: const Icon(
                 Icons.person,
-                size: 40,
+                size: 50,
                 color: Colors.black,
               ),
             ),
             decoration: const BoxDecoration(
               color: Color(0xFF1225F5),
+              gradient: LinearGradient(
+                colors: [Color(0xFF1225F5), Color(0xFF4A8DFF)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
           // Opciones del Drawer
@@ -40,15 +46,15 @@ class CustomDrawer extends StatelessWidget {
             child: ListView(
               padding: EdgeInsets.zero,
               children: [
+                // Botón para ir al sitio web
                 ListTile(
-                  leading: const Icon(Icons.school),
-                  title: Text(
-                      'Carrera: ${userData['career'] ?? 'Sin carrera registrada'}'),
-                ),
-                ListTile(
-                  leading: const Icon(Icons.calendar_month),
-                  title: Text(
-                      'Semestre: ${userData['semestre'] ?? 'Sin semestre registrado'}'),
+                  leading:
+                      const Icon(Icons.open_in_browser, color: Colors.indigo),
+                  title: const Text(
+                    'Académico UNIBE',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  onTap: () => _launchURL(context),
                 ),
               ],
             ),
@@ -57,8 +63,11 @@ class CustomDrawer extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListTile(
-              leading: const Icon(Icons.exit_to_app),
-              title: const Text('Cerrar Sesión'),
+              leading: const Icon(Icons.exit_to_app, color: Colors.red),
+              title: const Text(
+                'Cerrar Sesión',
+                style: TextStyle(fontSize: 16),
+              ),
               onTap: () {
                 FirebaseAuth.instance.signOut();
                 Navigator.of(context).pushReplacement(
@@ -68,10 +77,28 @@ class CustomDrawer extends StatelessWidget {
               tileColor: Colors.grey[200],
               textColor: Colors.black,
               iconColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void _launchURL(BuildContext context) async {
+    const url = 'https://acad.unibe.edu.ec/';
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('No se puede abrir $url'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_localizations/flutter_localizations.dart'; // Import necesario
 import 'package:provider/provider.dart';
-import 'login/users_provider.dart';
-import 'firebase_options.dart';
-import 'login/login_screen.dart';
+import 'package:unibe_app_control/home/home_screen.dart';
+import 'package:unibe_app_control/login/login_screen.dart';
+import 'package:unibe_app_control/login/users_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UsuarioProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Unibe Identidad',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UsuarioProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Unibe App Control',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const InitialScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('es', 'ES'), // Español
-        Locale('en', 'US'), // Inglés
-      ],
-      home: const InitialScreen(),
     );
   }
 }
-
 
 class InitialScreen extends StatelessWidget {
   const InitialScreen({super.key});
@@ -60,26 +43,18 @@ class InitialScreen extends StatelessWidget {
           children: <Widget>[
             Image.asset(
               'images/logo_unibe.png',
-              width: MediaQuery.of(context).size.width *
-                  0.8,
+              width: MediaQuery.of(context).size.width * 0.8,
               fit: BoxFit.contain,
             ),
             const SizedBox(height: 150),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF1225F5),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding:
-                    const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
               ),
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          const LoginScreen()),
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
                 );
               },
               child: const Text(
